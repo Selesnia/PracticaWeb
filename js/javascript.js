e0f0130b8bd8d821315da69eb937ba0d908f86dc
Selesnia <madaranin@icloud.com>
2025-06-10 03:40:53 -0300

    agregue link de enlace al whatsapp brayan

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
 function hhh_brayan() {
-    alert("Dale papu que estas esperando(+5493462387810)!");
+    alert("Dale papu que estas esperando(+5493462387810)!
+        window.location.href = "https://wa.me/5493462387810?text=Hola%20Cajero%20Francisco!%20Quiero%20hacerme%20un%20usuario%20con%20vos%20y%20cargar.%20";);
 }
